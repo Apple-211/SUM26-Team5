214e237b8cb097492243e2dfdeaa30b7f82b28ff
adcrespo-a <adcrespo@uncg.edu>
2026-06-29 19:18:01 -0400

    Add files via upload

---
+++ backend-api/spring_demo/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+	}
+
+}
