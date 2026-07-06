11d41e8de7ae4cfc6d1311284a2ad8b98d35eaa6
Yasmin <yacastrocer@uncg.edu>
2026-07-06 10:54:47 -0400

    Added scaffolding files

---
+++ backend-api/src/main/java/com/CSC340/HobbyCircles/HobbyCirclesApplication.java
@@ -0,0 +1,13 @@
+package com.CSC340.HobbyCircles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyCirclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyCirclesApplication.class, args);
+	}
+
+}
---
+++ backend-api/src/test/java/com/CSC340/HobbyCircles/HobbyCirclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340.HobbyCircles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyCirclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
