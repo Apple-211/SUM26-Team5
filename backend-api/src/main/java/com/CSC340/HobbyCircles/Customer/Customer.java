641a1cff43813ca752f61aaa39d415e6203fcb57
adcrespo-a <adcrespo@uncg.edu>
2026-07-08 11:35:14 -0400

    commit

---
+++ backend-api/src/main/java/com/CSC340/HobbyCircles/Customer/Customer.java
@@ -0,0 +1,34 @@
+package com.CSC340.HobbyCircles.Customer;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String password;
+    private String photoUrl;
+    private String hobbies;
+    private String location;
+    private String accountStatus;
+}
